670ef9b3c0835801f8aae536ec356d7e9ac9aff4
¨stefania¨ <estefaniahurtadog@javerianacali.edu.co>
2024-03-14 14:42:40 -0500

    roles

--- pro/login_registrar.php
+++ pro/login_registrar.php
@@ -1,52 +1,54 @@
-<!--
-Crear un login y una página para registrar a un nuevo usuario con los siguientes datos:
-usuario y contraseña.
-
-
-(index.html) Login 	  -> Si el usuario existe 	 -> principal.html
-(index.html) Login    -> Si el usuario no existe -> (index.html) Login
- registrar.html       -> Nuevo usuario           -> (index.html) Login
-
-El login tendrá la opción para registrar.
-Usar "estilos.css" en el ejercicio.
-
--->
-
 <?php
 
 include("conexion.php");
 
 $nombre = $_POST["usuario"];
 $pass   = $_POST["pass"];
+$tipoUsuario = $_POST["tipoUsuario"];
 
 //Login
 if(isset($_POST["btningresar"]))
 {
-	$query = mysqli_query($conn,"SELECT * FROM login WHERE usuario = '$nombre' AND password='$pass'");
-	$nr = mysqli_num_rows($query);
-	
-	if($nr==1)
-	{
-		echo "<script> alert('Bienvenido $nombre'); window.location='principal.html' </script>";
-	}else
-	{
-		echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
-	}
+    $query = mysqli_query($conn,"SELECT * FROM login WHERE Usuario = '$nombre' AND Password='$pass' AND idTipoUsuario = $tipoUsuario");
+    $nr = mysqli_num_rows($query);
+    
+    if($nr == 1)
+    {
+        $usuario = mysqli_fetch_assoc($query);
+        $idTipoUsuario = $usuario['idTipoUsuario'];
+        
+        if($idTipoUsuario == 1)
+        {
+            echo "<script> alert('Bienvenido $nombre'); window.location='admin.html' </script>";
+        }
+        elseif($idTipoUsuario == 2)
+        {
+            echo "<script> alert('Bienvenido $nombre'); window.location='cliente.html' </script>";
+        }
+        else
+        {
+            echo "<script> alert('Tipo de usuario desconocido'); window.location='index.html' </script>";
+        }
+    }
+    else
+    {
+        echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
+    }
 }
 
 //Registrar
 if(isset($_POST["btnregistrar"]))
 {
-	$sqlgrabar = "INSERT INTO login(usuario,password) values ('$nombre','$pass')";
-	
-	if(mysqli_query($conn,$sqlgrabar))
-	{
-		echo "<script> alert('Usuario registrado con exito: $nombre'); window.location='index.html' </script>";
-	}else 
-	{
-		echo "Error: ".$sql."<br>".mysql_error($conn);
-	}
+    $sqlgrabar = "INSERT INTO login(Usuario, Password, idTipoUsuario) VALUES ('$nombre','$pass','$tipoUsuario')";
+    
+    if(mysqli_query($conn,$sqlgrabar))
+    {
+        echo "<script> alert('Usuario registrado con éxito: $nombre'); window.location='index.html' </script>";
+    }
+    else 
+    {
+        echo "Error: ". $sqlgrabar ."<br>". mysqli_error($conn);
+    }
 }
-
 
 ?>
